34826fb88997fbd4f4cc2dcc92282383167b83cb
Omkar sanganwar <110180378+Omkar-sanganwar@users.noreply.github.com>
2024-11-27 14:40:37 +0530

    add getmapping and Postmapping and Request
    add getmapping and Postmapping and Request  ,client is a damee postman Application to be executed the code using url,localhost.8080/

--- ECIController.java
+++ ECIController.java
@@ -2,7 +2,32 @@ package ECIController;
 
 import com.grp17.firstwebsite.FirstwebsiteApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+// Request body POJO class to accept numbers
+class AdditionRequest {
+    private int num1;
+    private int num2;
+
+    // Getters and setters
+    public int getNum1() {
+        return num1;
+    }
+
+    public void setNum1(int num1) {
+        this.num1 = num1;
+    }
+
+    public int getNum2() {
+        return num2;
+    }
+
+    public void setNum2(int num2) {
+        this.num2 = num2;
+    }
+}
 
 @RestController
 public class ECIController {
@@ -10,6 +35,12 @@ public class ECIController {
     @GetMapping("/add")
     public String addTwoNumbers(int num1, int num2) {
         return FirstwebsiteApplication.addTwo(num1, num2);
+    }
+
+    // POST method for /add endpoint
+    @PostMapping("/add")
+    public String addTwoNumbersPost(@RequestBody AdditionRequest request) {
+        return FirstwebsiteApplication.addTwo(request.getNum1(), request.getNum2());
     }
 
     @GetMapping("/subtract")
